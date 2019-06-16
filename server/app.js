86aaa91caf7dc3b69277e4d8f1ee3d2765a3df95
Ladapo Samuel <ladaposamuel@outlook.com>
2019-06-16 18:30:04 +0100

    Setup linting library

--- server/app.js
+++ server/app.js
@@ -1,13 +1,11 @@
 // app.js
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'Welcome to PropertyPro Lite API'});
-})
+app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to PropertyPro Lite API' }));
 
-app.listen(3000)
-console.log('app running on port ', 3000);
+app.listen(3000);
+// console.log('app running on port ', 3000);
