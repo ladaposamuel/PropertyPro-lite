796e81bf9dd810cc991097958ea521e9ee348953
ladapo samuel <ladaposamuel@outlook.com>
2019-06-21 15:23:55 +0100

    documentation tests cases

--- server/test/docs.test.js
+++ server/test/docs.test.js
@@ -5,23 +5,20 @@ import server from '../app';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('Verify API documention is live', () => {
+describe('Verify test framework is set up properly', () => {
   it('should verify test framwork is set up by passing', (done) => {
-    chai
-      .request(server)
-      .get('/swagger.json')
-      .end((err, res) => {
-        expect(res.status).to.eql(200);
-        done();
-      });
+    expect(200).to.eql(200);
+    done();
   });
-  it('verify docs route is active', (done) => {
+});
+
+describe('Test index page', () => {
+  it('should test that the index page is responding', () => {
     chai
       .request(server)
-      .get('/docs')
+      .get('/')
       .end((err, res) => {
         expect(res.status).to.eql(200);
-        done();
       });
   });
 });
