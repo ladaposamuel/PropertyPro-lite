9ae5c5758aecdf30e37badc8bd9e2583b3280417
ladapo samuel <ladaposamuel@outlook.com>
2019-07-03 13:06:34 +0100

    wrote authentication route documentations

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,12 +11,50 @@ const router = express.Router();
  *     properties:
  *       email:
  *         type: string
- *         required: true
  *       password:
  *         type: string
  *       firstName:
  *         type: string
  *       lastName:
+ *         type: string
+ *       phonenNumber:
+ *         type: string
+ *       address:
+ *         type: string
+ *       is_Admin:
+ *         type: boolean
+ *       is_Agent:
+ *         type: boolean
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Login:
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Request:
+ *     properties:
+ *       email:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   Reset:
+ *     properties:
+ *       email:
+ *         type: string
+ *       password:
  *         type: string
  */
 
@@ -43,6 +81,75 @@ const router = express.Router();
  *         description: Email already in use
  *       404:
  *         description: Email not found
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Sign in an account
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: user object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Login'
+ *     responses:
+ *       200:
+ *         description: token
+ *       400:
+ *         description: Invalid email or password
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/reset:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Request for password reset
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: user object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Request'
+ *     responses:
+ *       200:
+ *         description: Email sent successfully
+ *       400:
+ *         description: Invalid email
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/new-password:
+ *   post:
+ *     tags:
+ *       - Authentication
+ *     description: Reset user's password
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: user
+ *         description: user object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Reset'
+ *     responses:
+ *       200:
+ *         description: Password reset successfully
+ *       400:
+ *         description: Invalid password
  */
 
 router.post('/signup', validator.signUpValidation, UserController.create);
