9da95193a094a0aa0847df90ddfd4f6405b460ae
ladapo samuel <ladaposamuel@outlook.com>
2019-07-09 18:21:19 +0100

    created password reset controller and email

--- server/mail_templates/password_reset.js
+++ server/mail_templates/password_reset.js
@@ -17,7 +17,7 @@ const emailTemplate = (priority, url, title, year) => `
   </div>
 </div>`;
 
-export const resetPasswordEmailTemplate = (url, resetToken, email, year) => `
+export const resetPasswordEmailTemplate = (newPassword, year) => `
 <div style="background: #fff; width: 100%; height: 100%; padding: 3rem 0; font-family: 'Georgia'">
   <div style="width: 50vw; background: #FFF; display: block; margin: 0 auto; padding: 0; border: 1px solid #a00b40;">
     <div style=" background: #a00b40;">
@@ -26,19 +26,12 @@ export const resetPasswordEmailTemplate = (url, resetToken, email, year) => `
     <div style="text-align: center; padding: 0.5rem;">
       <p style="font-size: 1rem;">Hello there,</p>
       <p style="font-size: 1rem;">You recently requested a password reset
-      on your PropertyPro-Lite account. Click the button below to reset it</p>
+      on your PropertyPro-Lite account. Below is your new password be sure to change it after successful login.</p>
       <p style="padding: 0.3rem;"></p>
-      <a style="padding: 0.7rem 2rem; background: #a00b40; color: white; text-decoration: none; border-radius: 2px;" href="http://${url}/resetpassword/?token=${resetToken}&email=${email}">RESET PASSWORD</a>
+      <hr>
+      <center>${newPassword}</center>
+      <hr>
       <p style="padding: 0.3rem;"></p>
-      <p style="padding: 1rem; font-size: 0.8rem;">If you did not request
-      a password reset, please ignore this email.
-      This password reset is only valid for the next 24 hours.</p>
-      <hr /> <p style="padding: 0.3rem;"></p>
-      <p style="padding: 1rem; font-size: 0.8rem;">If you're having trouble
-      clicking the password button, copy and paste the URL below into your
-      browser</p>
-      <p style="padding: 1rem; font-size: 0.8rem;">Link: http://${url}/resetpassword/?token=${resetToken}&email=${email}</p>
-      <p style="padding: 0.3rem;"></p> <hr/>
       <p style="font-size: 1rem;">PropertyPro-Lite © ${year}</p>
     </div>
   </div>
